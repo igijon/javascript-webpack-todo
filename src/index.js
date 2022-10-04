37ecf79b2ec843b9a5b42b81783ff68ce9d325d1
Inma Gijón <igijoninf@gmail.com>
2022-10-04 12:18:36 +0200

    Evento para crear un Todo

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './styles.css';
 import { Todo, TodoList } from './classes';
 import { crearTodoHtml } from './js/componentes';
 
-const todoList = new TodoList();
+export const todoList = new TodoList();
 
 const tarea = new Todo('Aprender JavaScrip!!');
 
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,6 +1,9 @@
+import {Todo} from '../classes';
+import {todoList} from '../index';
+
 //Referencias en el HTML
 const divTodoList = document.querySelector('.todo-list');
-
+const txtInput = document.querySelector('.new-todo');
 
 export const crearTodoHtml = (todo) => {
     const htmlTodo = `
@@ -22,3 +25,13 @@ export const crearTodoHtml = (todo) => {
 
     return div;
 }
+
+//Eventos
+txtInput.addEventListener('keyup', (event) => {
+    if(event.keyCode === 13 && txtInput.value.length > 0) { //Cuando pulso intro
+        const nuevoTodo = new Todo(txtInput.value);
+        todoList.nuevoTodo(nuevoTodo);
+        crearTodoHtml(nuevoTodo);
+        txtInput.value = '';
+    }
+});
